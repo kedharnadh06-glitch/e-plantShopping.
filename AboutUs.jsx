9329765a9c72bf8da0bcac2eb68e090f8b27eeb0
kedharnadh06-glitch <kedharnadh06@gmail.com>
2026-02-21 13:51:52 +0530

    Add AboutUs component with nursery information

---
+++ AboutUs.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function AboutUs() {
+  return (
+    <section>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery is dedicated to bringing the beauty of nature into your
+        home. We specialize in aromatic and medicinal plants that promote
+        wellness and create a calming environment.
+      </p>
+    </section>
+  );
+}
